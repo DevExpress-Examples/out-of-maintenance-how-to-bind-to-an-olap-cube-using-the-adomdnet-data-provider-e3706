7ee396f4cdf693d645984590c4d3bcbab82d12b0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_ADOMD/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+
+namespace DXPivotGrid_ADOMD {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VB/DXPivotGrid_ADOMD/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
+
+Namespace DXPivotGrid_ADOMD
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
